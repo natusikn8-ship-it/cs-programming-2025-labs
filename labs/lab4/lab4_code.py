0faaf118a85228807e01c4607a6e89ddf50cb1bf
Your Name <natusikn8@gmail.com>
2025-10-17 09:40:23 +1000

    2

---
+++ labs/lab4/lab4_code.py
@@ -0,0 +1,8 @@
+#Задание 1
+temperature = float(input("Введите температуру в комнате: "))
+if temperature >= 20:
+    print(f"Температура {temperature}°C - Кондиционер: ВЫКЛЮЧЕН")
+else:
+    print(f"Температура {temperature}°C - Кондиционер: ВКЛЮЧЕН")
+
+#Задание 2
