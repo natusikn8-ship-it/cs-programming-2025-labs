39f3d1382c3d8a3decca45264d5a2a443cd450e2
Your Name <natusikn8@gmail.com>
2025-10-17 09:57:18 +1000

    4

--- labs/lab4/lab4_code.py
+++ labs/lab4/lab4_code.py
@@ -6,3 +6,17 @@ else:
     print(f"Температура {temperature}°C - Кондиционер: ВКЛЮЧЕН")
 
 #Задание 2
+month = int(input("Введите номер месяца от 1 до 12: "))   
+if 1 <= month <= 12:
+    if month in [12, 1, 2]:
+        x = "Зима"
+    elif month in [3, 4, 5]:
+        x = "Весна"
+    elif month in [6, 7, 8]:
+        x = "Лето"
+    elif month in [9, 10, 11]:
+        x = "Осень"
+        print(f"Месяц {month} относится к сезону: {x}")
+else:
+         print("Ошибка! Ввежите число от 1 до 12")
+        
