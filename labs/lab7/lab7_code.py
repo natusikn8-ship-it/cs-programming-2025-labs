4552d0273886b6ffa0ddb1f9a1689e37387ec438
Your Name <natusikn8@gmail.com>
2025-12-26 02:01:15 +1000

    77

--- labs/lab7/lab7_code.py
+++ labs/lab7/lab7_code.py
@@ -78,15 +78,6 @@ incidents = [
 ]
 print(sorted(incidents, key=lambda x: -x["staff"])[:3])
 
-#Задание 7
-incidents = [
-    {"id": 101, "staff": 4},
-    {"id": 102, "staff": 12},
-    {"id": 103, "staff": 7},
-    {"id": 104, "staff": 20}
-]
-print(sorted(incidents, key=lambda x: -x["staff"])[:3])
-
 #Задание 8
 protocols = [
     ("Lockdown", 5),
